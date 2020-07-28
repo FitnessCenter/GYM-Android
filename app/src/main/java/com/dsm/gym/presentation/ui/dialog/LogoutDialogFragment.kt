5d8e9c2703f56f5d5106ce0e171709022c6b5a85
Yujaemin <youjmen15@gmail.com>
2020-07-28 22:30:30 +0900

    [ADD]로그아웃 다이얼로그

---
+++ app/src/main/java/com/dsm/gym/presentation/ui/dialog/LogoutDialogFragment.kt
@@ -0,0 +1,21 @@
+package com.dsm.gym.presentation.ui.dialog
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.dsm.gym.R
+
+class LogoutDialogFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_logout_dialog, container, false)
+    }
+
+
+}
