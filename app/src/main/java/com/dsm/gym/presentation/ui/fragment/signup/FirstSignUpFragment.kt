005cf842d40eca8797ebd95cb0ba09a04aafc231
윤석준 <tjrwns8024@naver.com>
2020-08-12 10:56:08 +0900

    [MODIFY] register -> signup 네이밍 변경

--- app/src/main/java/com/dsm/gym/presentation/ui/fragment/signup/FirstSignUpFragment.kt
+++ app/src/main/java/com/dsm/gym/presentation/ui/fragment/signup/FirstSignUpFragment.kt
@@ -1,4 +1,4 @@
-package com.dsm.gym.presentation.ui.fragment.signup.complete
+package com.dsm.gym.presentation.ui.fragment.signup
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,13 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import com.dsm.gym.R
 
-class CompleteRegisterFragment : Fragment() {
+class FirstSignUpFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_complete_signup, container, false)
+        return inflater.inflate(R.layout.fragment_first_signup, container, false)
     }
 }
--- app/src/main/java/com/dsm/gym/presentation/ui/fragment/signup/SecondSignUpFragment.kt
+++ app/src/main/java/com/dsm/gym/presentation/ui/fragment/signup/SecondSignUpFragment.kt
@@ -7,7 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.dsm.gym.R
 
-class SecondRegisterFragment : Fragment() {
+class SecondSignUpFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
