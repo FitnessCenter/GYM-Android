f58b9e199f106b616bdd6a9c3f1c5ca33dc08ccc
윤석준 <tjrwns8024@naver.com>
2020-08-12 10:53:19 +0900

    [UPDATE] signInFragment 구현

--- app/src/main/java/com/dsm/gym/presentation/ui/fragment/signin/SignInFragment.kt
+++ app/src/main/java/com/dsm/gym/presentation/ui/fragment/signin/SignInFragment.kt
@@ -1,19 +1,35 @@
 package com.dsm.gym.presentation.ui.fragment.signin
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.navigation.Navigation.findNavController
 import com.dsm.gym.R
+import com.dsm.gym.databinding.FragmentSigninBinding
+import com.dsm.gym.presentation.base.EndPointDataBindingFragment
+import com.dsm.gym.presentation.viewmodel.signin.SignInViewModel
+import kotlinx.android.synthetic.main.fragment_signin.*
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class SignInFragment : Fragment() {
+class SignInFragment : EndPointDataBindingFragment<FragmentSigninBinding>() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_signin, container, false)
+    override val layoutId: Int
+        get() = R.layout.fragment_signin
+
+    override val viewModel: SignInViewModel by viewModel()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.vm = viewModel
     }
+
+    override fun observeEvent() {
+        viewModel.startSignUpEvent.observe(this, Observer {
+            findNavController(view!!).navigate(R.id.action_signInFragment_to_firstRegisterFragment)
+        })
+        viewModel.idErrorEvent.observe(this, Observer { login_id_edit_layout.error = it })
+
+        viewModel.passwordErrorEvent.observe(this, Observer { login_pw_edit_layout.error = it })
+    }
+
 }
