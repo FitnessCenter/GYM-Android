5771ee63c70e39719f333edeacd9eaf31f670e76
Yujaemin <youjmen15@gmail.com>
2020-08-12 14:14:37 +0900

    [UPDATE] dialog update

--- app/src/main/java/com/dsm/gym/presentation/ui/dialog/LogoutDialogFragment.kt
+++ app/src/main/java/com/dsm/gym/presentation/ui/dialog/LogoutDialogFragment.kt
@@ -1,21 +1,45 @@
 package com.dsm.gym.presentation.ui.dialog
 
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import androidx.navigation.findNavController
 import com.dsm.gym.R
+import kotlinx.android.synthetic.main.fragment_logout_dialog.view.*
 
-class LogoutDialogFragment : Fragment() {
+
+class LogoutDialogFragment : DialogFragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_logout_dialog, container, false)
+        val view = inflater.inflate(R.layout.fragment_logout_dialog, container,false)
+        dialog!!.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        return view
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.logout_confirm_tv.setOnClickListener {
+            requireActivity().findNavController(R.id.account_service_container).navigate(R.id.action_mainFragment_to_signInFragment)
+            dialog!!.dismiss()
+        }
+        view.logout_cancel_tv.setOnClickListener {
+            dialog!!.dismiss()
+        }
+    }
+
+
+
+
 
 
 }
