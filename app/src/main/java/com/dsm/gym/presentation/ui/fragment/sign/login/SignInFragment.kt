a491f599d02bb9dcaa3bd5f9eae1e5168944ff72
Yujaemin <youjmen15@gmail.com>
2020-08-17 21:11:04 +0900

    [ADD] SignIn to Main fragment

--- app/src/main/java/com/dsm/gym/presentation/ui/fragment/sign/login/SignInFragment.kt
+++ app/src/main/java/com/dsm/gym/presentation/ui/fragment/sign/login/SignInFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.dsm.gym.R
+import kotlinx.android.synthetic.main.fragment_login.*
 
 class SignInFragment : Fragment() {
 
@@ -16,5 +18,12 @@ class SignInFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        login_btn.setOnClickListener {
+            findNavController().navigate(R.id.action_signInFragment_to_mainFragment)
+        }
     }
 }
