75116eb8c430af8bffbd7b3c8845cd3ec4e6e363
Yujaemin <youjmen15@gmail.com>
2020-09-08 17:14:17 +0900

    [ADD] ApplyExerciseModule

---
+++ app/src/main/java/com/dsm/gym/presentation/di/module/presentation/account/ApplyExerciseModule.kt
@@ -0,0 +1,11 @@
+package com.dsm.gym.presentation.di.module.presentation.account
+
+
+
+import com.dsm.gym.presentation.viewmodel.applyexercise.ApplyExerciseViewModel
+import org.koin.core.module.Module
+import org.koin.dsl.module
+
+val applyExerciseModule: Module = module {
+    single { ApplyExerciseViewModel() }
+}
