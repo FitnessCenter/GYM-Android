8cc6c3eeae033a8cc8fbcd8a14209368a7dda3ca
윤석준 <tjrwns8024@naver.com>
2020-09-08 10:33:00 +0900

    [MODIFY] AuthRepositoryImpl 변경

--- app/src/main/java/com/dsm/gym/data/repository/AuthRepositoryImpl.kt
+++ app/src/main/java/com/dsm/gym/data/repository/AuthRepositoryImpl.kt
@@ -2,6 +2,8 @@ package com.dsm.gym.data.repository
 
 import android.util.Log
 import com.dsm.gym.data.datasource.AuthDataSource
+import com.dsm.gym.data.entity.toDataEntity
+import com.dsm.gym.data.entity.toEntity
 import com.dsm.gym.data.mapper.AuthDataMapper
 import com.dsm.gym.data.mapper.TokenDataMapper
 import com.dsm.gym.data.mapper.UserDataMapper
@@ -9,26 +11,22 @@ import com.dsm.gym.domain.entity.AuthEntity
 import com.dsm.gym.domain.entity.TokenEntity
 import com.dsm.gym.domain.entity.UserEntity
 import com.dsm.gym.domain.repository.AuthRepository
+import io.reactivex.Completable
 import io.reactivex.Flowable
+import io.reactivex.Single
 
 class AuthRepositoryImpl(
-    private val authDataSource: AuthDataSource,
-    private val authDataMapper: AuthDataMapper,
-    private val tokenDataMapper: TokenDataMapper,
-    private val userDataMapper: UserDataMapper
+    private val authDataSource: AuthDataSource
 ) : AuthRepository {
-    override fun signIn(auth: AuthEntity): Flowable<TokenEntity> {
-        Log.d("authRepo",auth.toString())
-        return authDataSource.postSignIn(authDataMapper.mapFrom(auth)).map { tokenDataMapper.mapFrom(it) }
-    }
+    override fun signIn(auth: AuthEntity): Single<TokenEntity>
+        = authDataSource.postSignIn(auth.toDataEntity()).map { it.toEntity() }
 
+    override fun signUp(user: UserEntity): Completable =
+        authDataSource.postSignUp(user.toDataEntity())
 
-    override fun signUp(user: UserEntity): Flowable<TokenEntity> =
-        authDataSource.postSignUp(userDataMapper.mapFrom(user)).map { tokenDataMapper.mapFrom(it) }
+    override fun saveToken(token: String, isAccess: Boolean) =
+        authDataSource.saveToken(token, isAccess)
 
-    override fun saveToken(token: String) =
-        authDataSource.saveToken(token)
-
-    override fun getToken(): String =
-        authDataSource.getToken()
+    override fun getToken(isAccess: Boolean): String =
+        authDataSource.getToken(isAccess)
 }
