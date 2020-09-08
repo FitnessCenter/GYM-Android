6aa53397a1b2fed9a1e0a2a416457fb2639d3af0
윤석준 <tjrwns8024@naver.com>
2020-09-08 10:08:56 +0900

    [ADD] ErrorHandler

--- app/src/main/java/com/dsm/gym/domain/handler/EquipmentErrorHandler.kt
+++
@@ -1,9 +0,0 @@
-package com.dsm.gym.domain.handler
-
-import com.dsm.gym.domain.base.ErrorHandlerEntity
-
-interface EquipmentErrorHandler{
-    fun getAllEquipmentErrorHandle(throwable: Throwable): ErrorHandlerEntity
-
-    fun postDetailEquipmentErrorHandle(throwable: Throwable): ErrorHandlerEntity
-}
