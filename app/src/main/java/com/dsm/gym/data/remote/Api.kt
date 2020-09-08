f29c24f8dd0fa285817aedbd255f783142aa7d64
윤석준 <tjrwns8024@naver.com>
2020-09-08 10:32:04 +0900

    [MODIFY] 반환형 변경

--- app/src/main/java/com/dsm/gym/data/remote/Api.kt
+++ app/src/main/java/com/dsm/gym/data/remote/Api.kt
@@ -3,17 +3,17 @@ package com.dsm.gym.data.remote
 import io.reactivex.Flowable
 import com.dsm.gym.data.entity.*
 import io.reactivex.Completable
-import retrofit2.Response
+import io.reactivex.Single
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface Api {
     @POST("auth")
-    fun signIn(@Body body: Any?): Flowable<TokenData>
+    fun signIn(@Body body: Any?): Single<TokenData>
 
     @POST("account")
-    fun signUp(@Body body: Any?): Flowable<TokenData>
+    fun signUp(@Body body: Any?): Completable
 
     @POST("/equipment-applies")
     fun postDetailEquipment(@Body body: Any?): Completable
