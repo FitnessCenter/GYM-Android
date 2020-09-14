1b02953ae9fd168b6de9ae8327545809cd9c8598
Yujaemin <youjmen15@gmail.com>
2020-09-14 13:24:18 +0900

    [ADD] applyexercise api

--- app/src/main/java/com/dsm/gym/data/remote/Api.kt
+++ app/src/main/java/com/dsm/gym/data/remote/Api.kt
@@ -2,10 +2,12 @@ package com.dsm.gym.data.remote
 
 import io.reactivex.Flowable
 import com.dsm.gym.data.entity.*
+import io.reactivex.Completable
 import io.reactivex.Single
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface Api {
     @POST("users/accounts/auth")
@@ -14,6 +16,9 @@ interface Api {
     @POST("users/accounts")
     fun signUp(@Body body: Any?): Flowable<TokenData>
 
-    @POST("")
-    fun applyExercise()
+    @POST("/exercise-applies/{time}/apply")
+    fun applyExercise(@Path("time") time : Int) : Completable
+
+    @POST("/exercise-applies/{time}/apply")
+    fun getAppliedExercisePersonnel(@Path("time") time : Int) : Single<List<UserData>>
 }
