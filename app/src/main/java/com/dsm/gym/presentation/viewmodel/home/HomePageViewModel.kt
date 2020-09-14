d09cf44bb0bb4e31bd6915a86fd19844fa18d039
윤석준 <tjrwns8024@naver.com>
2020-09-14 19:01:25 +0900

    [ADD] GetUserInfo ViewModel

--- app/src/main/java/com/dsm/gym/presentation/viewmodel/home/HomePageViewModel.kt
+++ app/src/main/java/com/dsm/gym/presentation/viewmodel/home/HomePageViewModel.kt
@@ -1,18 +1,18 @@
 package com.dsm.gym.presentation.viewmodel.home
 
-import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import com.dsm.gym.domain.base.Message
 import com.dsm.gym.domain.base.Result
 import com.dsm.gym.domain.entity.ExerciseTimeEntity
-import com.dsm.gym.domain.entity.toModel
+import com.dsm.gym.domain.entity.UserInfoEntity
 import com.dsm.gym.domain.usecase.GetNumOfDaysExerciseUseCase
+import com.dsm.gym.domain.usecase.GetUserInfoUseCase
 import com.dsm.gym.presentation.base.BaseViewModel
-import com.dsm.gym.presentation.model.ExerciseTimeModel
 import io.reactivex.observers.DisposableSingleObserver
 
 class HomePageViewModel(
-    private val useCase: GetNumOfDaysExerciseUseCase
+    private val getNumOfDaysExerciseUseCase: GetNumOfDaysExerciseUseCase,
+    private val getUserInfoUseCase: GetUserInfoUseCase
 ) : BaseViewModel() {
     val userNameText = MutableLiveData<String>()
     val exerciseTimeText = MutableLiveData<String>()
@@ -20,42 +20,51 @@ class HomePageViewModel(
     val randomText = MutableLiveData<String>()
 
     init {
-        Log.d("log",numOfDaysExerciseText.value!!)
+        getUserInfo()
         getNumOfDaysExercise()
     }
 
     private fun getNumOfDaysExercise() {
-        useCase.execute(Unit, object : DisposableSingleObserver<Result<ExerciseTimeEntity>>() {
+        getNumOfDaysExerciseUseCase.execute(Unit, object : DisposableSingleObserver<Result<ExerciseTimeEntity>>() {
             override fun onSuccess(result: Result<ExerciseTimeEntity>) {
                 when (result) {
-                    is Result.Success -> getSuccess(result.data.toModel())
-                    is Result.Error -> getFail(result)
+                    is Result.Success -> numOfDaysExerciseText.value = result.data.exerciseTime.toString()
+                    is Result.Error -> when (result.message) {
+                        Message.SERVER_ERROR -> createToastEvent.value = "서버 오류가 발생했습니다"
+
+                        Message.NETWORK_ERROR -> createToastEvent.value = "네트워크 오류가 발생했습니다"
+
+                        else -> createToastEvent.value = "알 수 없는 오류가 발생했습니다"
+                    }
                 }
             }
 
             override fun onError(e: Throwable) {
-                createToastEvent.value = "알 수 없는 에러가 발생했습니다"
+                createToastEvent.value = "알 수 없는 오가 발생했습니다"
             }
         })
     }
 
-    fun getSuccess(result: ExerciseTimeModel) {
-        Log.d("success",result.toString())
-        numOfDaysExerciseText.value = result.exerciseTime.toString()
-    }
+    private fun getUserInfo() {
+        getUserInfoUseCase.execute(Unit, object : DisposableSingleObserver<Result<UserInfoEntity>>(){
+            override fun onSuccess(result: Result<UserInfoEntity>) {
+                when(result){
+                    is Result.Success -> userNameText.value = result.data.studentName
+                    is Result.Error -> when (result.message) {
+                        Message.SERVER_ERROR -> createToastEvent.value = "서버 오류가 발생했습니다"
 
-    fun getFail(result: Result.Error<ExerciseTimeEntity>) {
-        when (result.message) {
-            Message.SERVER_ERROR -> createToastEvent.value = "서버 오류가 발생했습니다"
+                        Message.NETWORK_ERROR -> createToastEvent.value = "네트워크 오류가 발생했습니다"
 
-            Message.NETWORK_ERROR -> createToastEvent.value = "네트워크 오류가 발생했습니다"
+                        else -> createToastEvent.value = "알 수 없는 오류가 발생했습니다"
+                    }
+                }
+            }
 
-            Message.UNKNOWN_ERROR -> createToastEvent.value = "알 수 없는 오류가 발생했습니다"
+            override fun onError(e: Throwable) {
+                createToastEvent.value = "알 수 없는 오류가 발생했습니다"
+            }
 
-            Message.UNAUTHORIZED -> createToastEvent.value = "존재하지 않는 유저입니다"
-
-            else -> createToastEvent.value = "알 수 없는 오류가 발생했습니다"
-        }
+        })
     }
 
 }
