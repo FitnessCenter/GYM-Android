b7b3d2fc969713e6f83edde85cc8e07b3b45f744
윤석준 <tjrwns8024@naver.com>
2020-09-15 22:56:22 +0900

    [DELETE] SerializedName 어노테이션 제거

--- app/src/main/java/com/dsm/gym/data/entity/UserInfoData.kt
+++ app/src/main/java/com/dsm/gym/data/entity/UserInfoData.kt
@@ -1,12 +1,9 @@
 package com.dsm.gym.data.entity
 
 import com.dsm.gym.domain.entity.UserInfoEntity
-import com.google.gson.annotations.SerializedName
 
 data class UserInfoData(
-    @SerializedName("studentName")
     val studentName: String,
-    @SerializedName("studentNumber")
     val studentNumber: String
 )
 
