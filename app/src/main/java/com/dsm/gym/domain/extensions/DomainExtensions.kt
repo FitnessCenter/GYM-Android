95ec645de9d0518dc37d221a4b16e1544790978a
Yujaemin <youjmen15@gmail.com>
2020-09-16 18:23:49 +0900

    [ADD] 메시지 명시

--- app/src/main/java/com/dsm/gym/domain/extensions/DomainExtensions.kt
+++ app/src/main/java/com/dsm/gym/domain/extensions/DomainExtensions.kt
@@ -1,6 +1,6 @@
 package com.dsm.gym.domain.extensions
 
-import com.dsm.gym.domain.handler.ErrorHandler
+import com.dsm.gym.domain.base.ErrorHandler
 import com.dsm.gym.domain.base.Result
 import io.reactivex.Completable
 import io.reactivex.Single
@@ -9,7 +9,7 @@ fun <T> Single<T>.toResult(errorHandler: ErrorHandler): Single<Result<T>> =
     this.map {
         Result.Success(it) as Result<T>
     }.onErrorReturn {
-        Result.Error(errorHandler.errorHandle(it))
+        Result.Error(message = errorHandler.errorHandle(it))
     }
 fun Completable.toSingleResult(errorHandler: ErrorHandler): Single<Result<Unit>> =
     this.toSingle{}.toResult(errorHandler)
