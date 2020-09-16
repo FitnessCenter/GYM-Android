61255ea9070c78d1d975192acc54eaf27a2a6f79
Yujaemin <youjmen15@gmail.com>
2020-09-16 18:23:24 +0900

    [DELETE]중복 삭제

--- app/src/main/java/com/dsm/gym/domain/base/Result.kt
+++ app/src/main/java/com/dsm/gym/domain/base/Result.kt
@@ -10,12 +10,3 @@ sealed class Result<T> {
     ): Result<T>()
 
 }
-
-
-package com.dsm.gym.domain.base
-
-sealed class Result<T> {
-    data class Success<T>(val data: T) : Result<T>()
-
-    data class Error<T>(val message: Message) : Result<T>()
-}
