718a8ba0945b2fcfa62f5502912bbbdb58d577cb
Yujaemin <youjmen15@gmail.com>
2020-09-21 11:56:45 +0900

    [ADD] 운동신청 bindingadapter

--- app/src/main/java/com/dsm/gym/presentation/bindingadapter/BasicBindingAdapter.kt
+++ app/src/main/java/com/dsm/gym/presentation/bindingadapter/BasicBindingAdapter.kt
@@ -3,10 +3,24 @@ package com.dsm.gym.presentation.bindingadapter
 import androidx.databinding.BindingAdapter
 import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.RecyclerView
+import com.dsm.gym.presentation.adapter.ApplyExercisePersonnelAdapter
 import com.dsm.gym.presentation.adapter.EquipmentListAdapter
 import com.dsm.gym.presentation.model.EquipmentListModel
+import com.dsm.gym.presentation.model.UserInfoModel
+import java.lang.Exception
 
 @BindingAdapter("equipmentItems")
 fun RecyclerView.bindEquipmentItems(equipmentItems: MutableLiveData<ArrayList<EquipmentListModel>>){
     (adapter as EquipmentListAdapter).setItem(equipmentItems)
+}
+
+@BindingAdapter("appliedPersonnelItems")
+fun RecyclerView.bindAppliedPersonnel(appliedPersonnelItems: MutableLiveData<List<UserInfoModel>>){
+    try {
+
+        (adapter as ApplyExercisePersonnelAdapter).updateData(appliedPersonnelItems)
+    }
+    catch (e : Exception){
+        e.printStackTrace()
+    }
 }
