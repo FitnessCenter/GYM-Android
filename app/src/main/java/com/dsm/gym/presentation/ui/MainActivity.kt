efab55c1d19a66693ed4ae98b35706027f432ace
Yujaemin <youjmen15@gmail.com>
2020-09-24 16:11:43 +0900

    [ADD] sharedviewmodel 명시

--- app/src/main/java/com/dsm/gym/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/dsm/gym/presentation/ui/MainActivity.kt
@@ -3,9 +3,13 @@ package com.dsm.gym.presentation.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.dsm.gym.R
+import com.dsm.gym.presentation.viewmodel.mypage.EquipmentHistoryViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 
 class MainActivity : AppCompatActivity() {
+    private val weatherViewModel by viewModel<EquipmentHistoryViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
