283a2264373c6fdb027624e816d8fca8b71f7863
Yujaemin <youjmen15@gmail.com>
2020-09-24 16:07:35 +0900

    [ADD] dialog boilerplatecode 추상 클래스에 넣음

--- app/src/main/java/com/dsm/gym/presentation/base/DataBindingDialogFragment.kt
+++ app/src/main/java/com/dsm/gym/presentation/base/DataBindingDialogFragment.kt
@@ -1,5 +1,7 @@
 package com.dsm.gym.presentation.base
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,6 +9,8 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.DialogFragment
+import androidx.lifecycle.Observer
+import splitties.toast.toast
 
 abstract class DataBindingDialogFragment<T : ViewDataBinding> : DialogFragment() {
 
@@ -14,6 +18,8 @@ abstract class DataBindingDialogFragment<T : ViewDataBinding> : DialogFragment()
     lateinit var binding: T
 
     abstract val layoutId: Int
+    abstract val viewModel: BaseViewModel
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = DataBindingUtil.inflate(inflater, layoutId, container, false)
@@ -22,4 +28,13 @@ abstract class DataBindingDialogFragment<T : ViewDataBinding> : DialogFragment()
 
         return rootView
     }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        dialog!!.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        observeEvent()
+        viewModel.createToastEvent.observe(viewLifecycleOwner, Observer { toast(it) })
+    }
+    abstract fun observeEvent()
+
+
 }
